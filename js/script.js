8eb3f7cf473c263e7c000cca97657ac2570ffa0c
Nurbek <malikov.n004@gmail.com>
2022-06-12 21:48:04 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,27 @@
+
+
+const arr = [
+    {
+      id: 1,
+      title: 'Nurbek'
+    },
+    {
+      id: 2,
+      title: 'Beka',
+    },
+    {
+      id: 3,
+      title: 'Ali',
+    },
+    {
+      id: 4,
+      title: 'Omad',
+    },
+    {
+      id: 5,
+      tilte: 'Urmat',
+    },
+]
+
+
+console.log(arr)
